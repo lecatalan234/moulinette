ac5d5433d25c77da3acbc0c3c5104dd9d7dceaa0
younes <manzahyounes@gmail.com>
2022-05-18 00:02:11 +0100

    modif 17052022 sonar2

--- src/main/java/ma/oncf/sfa/moulinette/utils/Constants.java
+++ src/main/java/ma/oncf/sfa/moulinette/utils/Constants.java
@@ -1,9 +1,34 @@
 package ma.oncf.sfa.moulinette.utils;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public interface Constants {
-    HashMap<String, String> SENS = new HashMap<String, String>(){{
+public class Constants {
+    public static Map<String, String> SENS;
+    static {
+        SENS = new HashMap<>();
+        SENS.put("{","C");
+        SENS.put("A","C");
+        SENS.put("B","C");
+        SENS.put("C","C");
+        SENS.put("D","C");
+        SENS.put("E","C");
+        SENS.put("F","C");
+        SENS.put("G","C");
+        SENS.put("H","C");
+        SENS.put("I","C");
+        SENS.put("}","D");
+        SENS.put("J","D");
+        SENS.put("K","D");
+        SENS.put("L","D");
+        SENS. put("M","D");
+        SENS.put("N","D");
+        SENS.put("O","D");
+        SENS.put("P","D");
+        SENS.put("Q","D");
+        SENS.put("R","D");
+    }
+/*    HashMap<String, String> SENS = new HashMap<String, String>(){{
         put("{","C");
         put("A","C");
         put("B","C");
@@ -24,10 +49,10 @@ public interface Constants {
         put("P","D");
         put("Q","D");
         put("R","D");
-    }};
+    }};*/
 
-    String UPLOAD_PATH = "./moulinetteFiles/uploads/";
+    public static String UPLOAD_PATH = "./moulinetteFiles/uploads/";
 
-    String DOWNLOAD_PATH = "./moulinetteFiles/downloads/";
+    public static String DOWNLOAD_PATH = "./moulinetteFiles/downloads/";
 
 }
